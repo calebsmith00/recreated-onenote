56f7b12dcfa809cdafbcf3f0fba008bd0daa6c1a
minifiedcode <minifiedcode@gmail.com>
2022-06-13 10:06:01 -0500

    Import fonts used for MaterialUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,10 @@
+/* MATERIAL UI IMPORT BEGIN */
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+/* MATERIAL UI IMPORT FINISH */
+
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
