0d2f590515399ed0dd962ac42d8d7c63e2129fea
minifiedcode <minifiedcode@gmail.com>
2022-06-22 14:41:04 -0500

    Update login component to receive authorization code from OpenID

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,26 @@
 import type { NextPage } from "next";
-import { Login } from "@microsoft/mgt-react";
+import { GetServerSideProps } from "next";
+const authorizeURL =
+  "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize";
 
 const LoginPage: NextPage = () => {
   return (
     <>
-      <Login />
+      <p>Login</p>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const APP_ID = process.env.ALT_APP_ID;
+  const response = await fetch(
+    `${authorizeURL}?client_id=${APP_ID}&response_type=code&redirect_uri=http://localhost:3000&response_mode=form_post&scope=user.read.all`
+  );
+  console.log(response.body);
+
+  return {
+    props: {},
+  };
 };
 
 export default LoginPage;
