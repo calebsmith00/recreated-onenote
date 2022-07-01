62086abeabe23ef9be46b3c5eeaaef6837c0e34a
Caleb Smith <minifiedcode@gmail.com>
2022-07-01 08:59:50 -0500

    Fix "View Template" page

--- components/template_view/index.tsx
+++ components/template_view/index.tsx
@@ -1,36 +1,98 @@
-import { MouseEvent } from "react";
+import { useEffect, useState } from "react";
+import SelectTemplate from "./select_template";
+
+export const genericFetchOptions: RequestInit = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  credentials: "include",
+  body: undefined,
+};
 
 function TemplateView() {
-  const getNotebook = async (event: MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
+  const [templates, setTemplates] = useState([]);
+  const [page, setPage] = useState();
+  useEffect(() => {
+    async function GetSection() {
+      const pageFetchOptions: RequestInit = {
+        ...genericFetchOptions,
+        body: JSON.stringify({
+          displayName: "Training List",
+        }),
+      };
 
-    const body = JSON.stringify({
-      displayName: "Master Notebooks",
-    });
+      try {
+        const response = await fetch(
+          "http://localhost:3000/api/retrieve/section",
+          pageFetchOptions
+        );
+        const section = await response.json();
+        return section;
+      } catch (e) {
+        console.log(`Error at GetSection(): ${e}`);
+        return false;
+      }
+    }
 
-    const options: RequestInit = {
-      headers: new Headers({
-        "Content-Type": "application/json",
-      }),
-      body,
-      method: "POST",
-      credentials: "include",
-    };
-    const notebook = await fetch(
-      "http://localhost:3000/api/retrieve/notebook",
-      options
-    );
+    async function GetPages() {
+      const section = await GetSection();
+      if (!section.id) return;
 
-    console.log(notebook);
-  };
+      const pageFetchOptions: RequestInit = {
+        ...genericFetchOptions,
+        body: JSON.stringify({
+          sectionID: section.id,
+        }),
+      };
+
+      const response = await fetch(
+        "http://localhost:3000/api/retrieve/pages",
+        pageFetchOptions
+      );
+      const pages = await response.json();
+
+      if (pages.length < 1) return;
+      setTemplates(pages);
+    }
+
+    async function GetPage() {
+      const section = await GetSection();
+
+      if (!section.id) return;
+
+      const pageFetchOptions: RequestInit = {
+        ...genericFetchOptions,
+        body: JSON.stringify({
+          sectionID: section.id,
+          title: "Level 1",
+        }),
+      };
+
+      try {
+        const response = await fetch(
+          "http://localhost:3000/api/retrieve/page",
+          pageFetchOptions
+        );
+        const page = await response.json();
+
+        setPage(page);
+      } catch (e) {
+        console.log(`Error at GetPage(): ${e}`);
+        return false;
+      }
+    }
+
+    GetPages();
+
+    return () => console.log("Data retrieved");
+  }, []);
 
   return (
     <>
-      <form>
-        <button type="submit" onClick={getNotebook}>
-          Get notebooks
-        </button>
-      </form>
+      <SelectTemplate templates={templates} />
     </>
   );
 }
+
+export default TemplateView;
